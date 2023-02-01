59c818e8d5f517b7299155e8e014ad89fc14a766
Usynin1994 <maximus616161@gmail.com>
2023-02-02 00:12:36 +0700

    Playlist Maker - Homework 1

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<View>(R.id.search_button).setOnClickListener {
+            startActivity(Intent(this, SearchActivity::class.java))
+        }
+
+        findViewById<View>(R.id.media_button).setOnClickListener {
+            startActivity(Intent(this, MediaActivity::class.java))
+        }
+
+        findViewById<View>(R.id.setting_button).setOnClickListener {
+            startActivity(Intent(this, SettingActivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/com/example/playlistmaker/SettingActivity.kt
@@ -0,0 +1,17 @@
+package com.example.playlistmaker
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class SettingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting)
+
+        findViewById<View>(R.id.go_back).setOnClickListener {
+            finish()
+        }
+    }
+}
